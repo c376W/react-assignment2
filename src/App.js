4f9e386fcfe641aa7193d8174dd2aed1cd02290b
c376W <mailfzc123@gmail.com>
2019-03-15 14:49:19 -0700

    creating lists.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
@@ -12,8 +13,22 @@ class App extends Component {
     this.setState({userInput: event.target.value});
    
   }
+
+  deleteCharHandler = ( index ) =>{
+    const text = this.state.userInput.split('');
+    text.splice(index,1);
+    const updatedText = text.join('');
+    this.setState({userInput:updatedText});
+
+  }
   
   render() {
+    const charList = this.state.userInput.split('').map((ch,index) =>{
+      return <Char 
+      character = {ch} 
+      key={index}
+      clicked = {()=>this.deleteCharHandler(index)}/>;
+    });
     return (
       <div >
         <input 
@@ -21,6 +36,8 @@ class App extends Component {
         onChange={this.inputChangedHandler} 
         value = {this.state.userInput}/>
         <p>{this.state.userInput}</p>
+        <Validation inputLength = {this.state.userInput.length}/>
+        {charList}
       </div>
     );
   }
--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const validation = (props)=>{
+    let validationMessage = 'Text long enough';
+    if (props.inputLength <=5){
+        validationMessage = 'Text too short';
+    }
     return(
         <div>
-            <p>Text too short!</p>
+            {/* {
+                props.inputLength > 5 ?
+                <p>Text long enough</p> :
+                <p>Text too short!</p>
+            } */}
+            <p>{validationMessage}</p>
         </div>
     )
 
