0204af3eb71a147ac4aed457de6185578ea878cb
c376W <mailfzc123@gmail.com>
2019-03-15 14:15:21 -0700

    Uploading files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Validation from './Validation/Validation';
 
 class App extends Component {
+  state = {
+    userInput:''
+  }
+
+  inputChangedHandler = (event) => {
+    this.setState({userInput: event.target.value});
+   
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <input 
+        type = "text"
+        onChange={this.inputChangedHandler} 
+        value = {this.state.userInput}/>
+        <p>{this.state.userInput}</p>
       </div>
     );
   }
---
+++ src/Validation/Validation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const validation = (props)=>{
+    return(
+        <div>
+            <p>Text too short!</p>
+        </div>
+    )
+
+}
+
+export default validation;
